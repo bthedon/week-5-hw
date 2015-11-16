0f92f8d8db52ee21be448eff333b241cc4c99560
Brian Donegan <brian.donegan@careerbuilder.com>
2015-11-15 23:33:54 -0500

    added categories, cleaned up View Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106180941) do
+ActiveRecord::Schema.define(version: 20151116041107) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -19,6 +26,13 @@ ActiveRecord::Schema.define(version: 20151106180941) do
     t.text     "comment_entered"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: :cascade do |t|
